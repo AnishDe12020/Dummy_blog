83d8f786bb20782a6a85f4d0ac28f32e3524dedb
AnishDe12020 <anishde2007@gmail.com>
2020-11-24 14:55:37 +0530

    Just did a lot of things including databases (models) and forms

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,6 +1,34 @@
+import os
 from flask import Flask 
+from flask_sqlalchemy import SLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
+
+#########################
+##### DATABASE SETUP ####
+#########################
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.slite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SLAlchemy(app)
+Migrate(app, db)
+
+#########################
+
+#########################
+##### LOGIN CONFIGS #####
+#########################
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "users.login"
+
+#########################
 
 from puppycompanyblog.core.views import core
 from puppycompanyblog.error_pages.handlers import error_pages
